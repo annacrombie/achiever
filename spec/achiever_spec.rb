5afba33ab73c6a290fa2cfb57e04eaca7116ea17
Stone Tickle <lattis@mochiro.moe>
2019-03-27 11:44:44 -0500

    these specs don't belong here

--- spec/achiever_spec.rb
+++ spec/achiever_spec.rb
@@ -1,20 +1,5 @@
 RSpec.describe Achiever do
-  context Achiever::Config do
-    it 'adds in default values' do
-      expect(Achiever.achievement(:logins)[:visibility]).to eq('visible')
-    end
-  end
-
-  context 'gettin achievements' do
-    before(:each) do
-      @user = User.create
-    end
-
-    it 'awards slotted achievements' do
-      @user.achieve(:profile, :profile_picture)
-
-      expect(@user.badges.length).to eq(1)
-      expect(@user.badges[0].attr[:name]).to eq('Picture Perfect')
-    end
+  it 'has a version' do
+    expect(Achiever::VERSION).not_to be_nil
   end
 end
