81e484f301d1031a6ba690319d3af4f7cb67ce91
Stone Tickle <lattis@mochiro.moe>
2019-04-01 14:41:32 -0500

    spec out schedule achievement feature and get 100% coverage

--- spec/features/scheduled_achievement_spec.rb
+++ spec/features/scheduled_achievement_spec.rb
@@ -21,4 +21,15 @@ RSpec.describe 'scheduling achievements' do
     expect(@user).to have_new_badges
     expect(@user.achievement(:phishing).scheduled_achievements).to be_empty
   end
+
+  it 'should also work with slotted achievements' do
+    @user.achieve(:profile, :profile_picture, on: Date.today.next_month).tag!(:profile)
+
+    expect(@user).not_to have_new_badges
+
+    Timecop.travel(Date.today.next_year)
+
+    expect(@user).to have_new_badges
+    expect(@user.new_badges.first.name).to eq('Picture Perfect')
+  end
 end
