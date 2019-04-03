8880a676552ed2c9f8e277ae0339b203a378c89b
Stone Tickle <lattis@mochiro.moe>
2019-04-03 09:26:27 -0500

    get tags working and add some method aliases

--- app/models/achiever/scheduled_achievement.rb
+++ app/models/achiever/scheduled_achievement.rb
@@ -1,24 +1,35 @@
 module Achiever
   class ScheduledAchievement < ApplicationRecord
+    class<<self
+      alias_method :cancel_all, :destroy_all
+    end
+    alias_method :cancel, :destroy
+
     belongs_to :achievement
+
+    scope(:tagged, lambda do |*tags|
+      where('tags LIKE ?', "%#{tags.map(&:to_sym).sort.uniq.join('%')}%")
+    end)
 
     def apply
       achievement.achieve_raw(payload)
       destroy
     end
 
-    def tags
+    def setup_tags
       @tags ||= self[:tags].nil? ? [] : self[:tags].split(',').map(&:to_sym)
     end
 
-    def tag(t)
-      tags << t.to_s
-      self[:tags] = tags.join(',')
+    def tag(*t)
+      setup_tags
+      @tags += t.map(&:to_sym)
+      @tags.sort!.uniq!
+      self[:tags] = @tags.join(',')
       self
     end
 
-    def tag!(t)
-      tag(t)
+    def tag!(*t)
+      tag(*t)
       save
       self
     end
---
+++ spec/models/achiever/scheduled_achievement_spec.rb
@@ -0,0 +1,34 @@
+RSpec.describe Achiever::ScheduledAchievement do
+  before do
+    @user = User.create
+    10.times do |i|
+      @user.achieve(:logins, on: Date.tomorrow + i).tag!('test', "d#{i}")
+    end
+
+    10.times do |i|
+      @user.achieve(:phishing, on: Date.tomorrow + i).tag!('test', 'phishing', "d#{i}")
+    end
+  end
+
+  after do
+    @user.scheduled_achievements.cancel_all
+  end
+
+  it 'can be gotten from the scheduled_achievements method' do
+    expect(@user.scheduled_achievements.count).to eq(20)
+  end
+
+  it 'can be filtered by achievement' do
+    expect(@user.scheduled_achievements(:logins).count).to eq(10)
+  end
+
+  it 'can be accessed via a tagged scope' do
+    expect(@user.scheduled_achievements.tagged('test').length).to eq(20)
+    expect(@user.scheduled_achievements.tagged(:test, :phishing).length).to eq(10)
+    expect(@user.scheduled_achievements.tagged(:phishing).length).to eq(10)
+    expect(@user.scheduled_achievements.tagged(:d2).length).to eq(2)
+    p(@user.scheduled_achievements.tagged(:d2))
+    p(@user.scheduled_achievements.tagged(:d2, :test))
+    expect(@user.scheduled_achievements.tagged(:d2, :test).length).to eq(2)
+  end
+end
