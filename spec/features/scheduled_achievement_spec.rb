d16636b75c0eeb46e34d16fde863efdc712bad1a
Stone Tickle <lattis@mochiro.moe>
2019-05-14 10:07:48 -0500

    use a new user model every time to recache values
    this is also a more realistic simulation of a real request

--- spec/features/scheduled_achievement_spec.rb
+++ spec/features/scheduled_achievement_spec.rb
@@ -1,35 +1,39 @@
 RSpec.describe 'scheduling achievements' do
+  def user
+    User.find(user_id)
+  end
+
+  let(:user_id) { User.create.id }
+
   before do
     Timecop.freeze(Date.today)
-
-    @user = User.create
   end
 
   after do
     Timecop.return
 
-    @user.destroy
+    user.destroy
   end
 
   it 'should let you schedule achievements' do
-    @user.achieve(:phishing, 2, on: Date.tomorrow).tag!(:p1)
+    user.achieve(:phishing, 2, on: Date.tomorrow).tag!(:p1)
 
-    expect(@user).not_to have_new_badges
+    expect(user).not_to have_new_badges
 
     Timecop.travel(Date.tomorrow)
 
-    expect(@user).to have_new_badges
-    expect(@user.achievement(:phishing).scheduled_achievements).to be_empty
+    expect(user).to have_new_badges
+    expect(user.achievement(:phishing).scheduled_achievements).to be_empty
   end
 
   it 'should also work with slotted achievements' do
-    @user.achieve(:profile, :profile_picture, on: Date.today.next_month).tag!(:profile)
+    user.achieve(:profile, :profile_picture, on: Date.today.next_month).tag!(:profile)
 
-    expect(@user).not_to have_new_badges
+    expect(user).not_to have_new_badges
 
     Timecop.travel(Date.today.next_year)
 
-    expect(@user).to have_new_badges
-    expect(@user.new_badges.first.name).to eq('Picture Perfect')
+    expect(user).to have_new_badges
+    expect(user.new_badges.first.name).to eq('Picture Perfect')
   end
 end
--- spec/lib/achiever/subject_spec.rb
+++ spec/lib/achiever/subject_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe Achiever::Subject do
-  before(:each) do
-    @user = User.create
+  let(:user_id) { User.create.id }
+
+  def user
+    User.find(user_id)
   end
 
   it 'validates its includer' do
@@ -34,66 +36,66 @@ RSpec.describe Achiever::Subject do
   end
 
   it 'can help you find achievements' do
-    @user.achieve(:logins)
-    expect(@user.achievement(:logins)).not_to be_nil
+    user.achieve(:logins)
+    expect(user.achievement(:logins)).not_to be_nil
   end
 
   it 'can help you find if a user has a achievement' do
-    expect(@user).not_to have_achievement(:logins)
-    @user.achieve(:logins)
-    expect(@user).to have_achievement(:logins)
+    expect(user).not_to have_achievement(:logins)
+    user.achieve(:logins)
+    expect(user).to have_achievement(:logins)
   end
 
   it 'raises an exception on an invalid achievement name' do
-    expect { @user.achieve(:acount_owner) }.to raise_exception(
+    expect { user.achieve(:acount_owner) }.to raise_exception(
       Achiever::Exceptions::InvalidAchievementName)
   end
 
   it 'raises an exception on an invalid slot name' do
-    expect { @user.achieve(:profile, :feohifeh) }.to raise_exception(
+    expect { user.achieve(:profile, :feohifeh) }.to raise_exception(
       Achiever::Exceptions::InvalidSlot)
   end
 
   it 'raises an exception on an invalid type' do
-    expect { @user.achieve(:profile, 1) }.to raise_exception(TypeError)
-    expect { @user.achieve(:logins, :eoih) }.to raise_exception(TypeError)
+    expect { user.achieve(:profile, 1) }.to raise_exception(TypeError)
+    expect { user.achieve(:logins, :eoih) }.to raise_exception(TypeError)
   end
 
   context '#achieve' do
     it 'will create achievements for you' do
-      expect(@user.achievement(:logins)).to be_nil
-      @user.achieve(:logins)
-      expect(@user.achievement(:logins)).not_to be_nil
+      expect(user.achievement(:logins)).to be_nil
+      user.achieve(:logins)
+      expect(user.achievement(:logins)).not_to be_nil
     end
 
     it 'will only create one per name' do
-      expect(@user.achievement(:logins)).to be_nil
-      @user.achieve(:logins)
-      @user.achieve(:logins)
-      expect(@user.achievements.where(name: :logins).count).to eq(1)
+      expect(user.achievement(:logins)).to be_nil
+      user.achieve(:logins)
+      user.achieve(:logins)
+      expect(user.achievements.where(name: :logins).count).to eq(1)
     end
 
     it 'awards slotted achievements' do
-      @user.achieve(:profile, :profile_picture)
+      user.achieve(:profile, :profile_picture)
 
-      expect(@user.badges.length).to eq(1)
-      expect(@user.badges[:profile].first.name).to eq('Picture Perfect')
+      expect(user.badges.length).to eq(1)
+      expect(user.badges[:profile].first.name).to eq('Picture Perfect')
     end
 
     it 'can notify you' do
-      @user.achieve(:profile, :profile_picture)
-      expect(@user).to have_new_badges
-      expect(@user.new_badges[0].name).to eq('Picture Perfect')
-      @user.clear_new_badges
-      expect(@user).not_to have_new_badges
+      user.achieve(:profile, :profile_picture)
+      expect(user).to have_new_badges
+      expect(user.new_badges[0].name).to eq('Picture Perfect')
+      user.clear_new_badges
+      expect(user).not_to have_new_badges
 
-      @user.achieve(:logins, 1)
-      expect(@user).to have_new_badges
-      expect(@user.new_badges[0].name).to eq('Roll Call')
-      @user.clear_new_badges
-      expect(@user).not_to have_new_badges
+      user.achieve(:logins, 1)
+      expect(user).to have_new_badges
+      expect(user.new_badges[0].name).to eq('Roll Call')
+      user.clear_new_badges
+      expect(user).not_to have_new_badges
 
-      expect(@user.badges.length).to eq(2)
+      expect(user.badges.length).to eq(2)
     end
 
     it 'can always get an achievement!' do
@@ -102,7 +104,7 @@ RSpec.describe Achiever::Subject do
           10.times.map do
             Thread.new do
               10.times do
-                expect { @user.achievement!(:logins) }.not_to raise_exception
+                expect { user.achievement!(:logins) }.not_to raise_exception
               end
             end
           end.map(&:join)
@@ -110,7 +112,7 @@ RSpec.describe Achiever::Subject do
         Thread.new do
           10.times.map do
             Thread.new do
-              10.times { @user.achievement!(:logins).delete }
+              10.times { user.achievement!(:logins).delete }
             end
           end.map(&:join)
         end
